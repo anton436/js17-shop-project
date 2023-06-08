3b262d8b3d4b4f2cb676977b29956882df2b3a6a
anton <oneshot436@gmail.com>
2023-06-08 20:01:32 +0600

    routes added

---
+++ src/routes/MainRoutes.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import AdminPage from "../pages/AdminPage";
+import HomePage from "../pages/HomePage";
+import NotFoundPage from "../pages/NotFoundPage";
+
+const MainRoutes = () => {
+  const PUBLIC_ROUTES = [
+    {
+      link: "/",
+      element: <HomePage />,
+      id: 1,
+    },
+    {
+      link: "/admin",
+      element: <AdminPage />,
+      id: 2,
+    },
+    {
+      link: "*",
+      element: <NotFoundPage />,
+      id: 3,
+    },
+  ];
+
+  return (
+    <Routes>
+      {PUBLIC_ROUTES.map((item) => (
+        <Route path={item.link} element={item.element} key={item.id} />
+      ))}
+    </Routes>
+  );
+};
+
+export default MainRoutes;
