f88a20d039063e2d41a87cd8704c21124a3acef7
lurzzznyaa <konkanekinnn@gmail.com>
2023-06-13 18:37:45 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React from 'react';
+import MainRoute from './routes/MainRoute';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Navbar />
+      <MainRoute />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
