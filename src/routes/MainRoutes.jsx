58b70fd9d2a2ed78297e72f42b94eef7263121cb
lurzzznyaa <konkanekinnn@gmail.com>
2023-06-13 20:38:10 +0600

    commit

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,42 +1,42 @@
-import React from "react";
-import HomePage from "../pages/HomePage";
-import AdminPage from "../pages/AdminPage";
-import NotFoundPage from "../pages/NotFoundPage";
-import { Route, Routes } from "react-router-dom";
-import ProductPage from "../pages/ProductPage";
-import ProductDetails from "../components/ProductDetails/ProductDetails";
-import EditProductPage from "../pages/EditProductPage";
+import React from 'react';
+import HomePage from '../pages/HomePage';
+import AdminPage from '../pages/AdminPage';
+import NotFoundPage from '../pages/NotFoundPage';
+import { Route, Routes } from 'react-router-dom';
+import ProductPage from '../pages/ProductPage';
+import ProductDetails from '../components/ProductDetails/ProductDetails';
+import EditProductPage from '../pages/EditProductPage';
 
 const MainRoutes = () => {
   const PUBLIC_ROUTES = [
     {
-      link: "/",
+      link: '/',
       element: <HomePage />,
       id: 1,
     },
     {
-      link: "/admin",
+      link: '/admin',
       element: <AdminPage />,
       id: 2,
     },
     {
-      link: "*",
+      link: '*',
       element: <NotFoundPage />,
       id: 3,
     },
 
     {
-      link: "/products",
+      link: '/products',
       element: <ProductPage />,
       id: 4,
     },
     {
-      link: "/edit/:id",
+      link: '/edit/:id',
       element: <EditProductPage />,
       id: 5,
     },
     {
-      link: "/details/:id",
+      link: '/details/:id',
       element: <ProductDetails />,
       id: 6,
     },
